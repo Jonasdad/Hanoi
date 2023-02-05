1f3b5064c4d056e7a858e7c9d0c531577d7b87fe
Jonas Dåderman <jonasdad@kth.se>
2023-02-05 18:02:32 +0100

    test commit

--- Hanoi.ex
+++ Hanoi.ex
@@ -1,5 +1,12 @@
 defmodule Hanoi do
-  def hanoi(n, a, b, c) do
-
+  def test() do
+    hanoi(4, "A", "B", "C")
   end
+
+  def hanoi(n, from, aux, to) do
+    hanoi(n-1, aux, from, to)
+    IO.puts()
+    hanoi(n-1, to, from, aux)
+  end
+  def hanoi(n, _from, _aux, _to) when n == 0 do IO.puts() end
 end
