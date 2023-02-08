f59710d240fe0acc5afc579951ec1a7e1f228a82
Jonas Dåderman <jonasdad@kth.se>
2023-02-08 12:35:56 +0100

    Finished

--- Hanoi.ex
+++ Hanoi.ex
@@ -1,12 +1,11 @@
 defmodule Hanoi do
   def test() do
-    hanoi(4, "A", "B", "C")
+    n = 3
+    min_moves = trunc(:math.pow(2, n) - 1)
+    IO.puts("Number of moves: #{min_moves}")
+    hanoi(n, :A, :B, :C)
   end
-
-  def hanoi(n, from, aux, to) do
-    hanoi(n-1, aux, from, to)
-    IO.puts()
-    hanoi(n-1, to, from, aux)
-  end
-  def hanoi(n, _from, _aux, _to) when n == 0 do IO.puts() end
+  def pprint(n, from, to) do IO.write("Disk #{n}, #{from} -> #{to}\n") end
+  def hanoi(n, from, _aux, to) when n == 1 do pprint(n, from, to) end
+  def hanoi(n, from, aux, to) do hanoi(n-1, from, to, aux); pprint(n, from, to); hanoi(n-1, aux, from, to) end
 end
